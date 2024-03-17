4c70960604acfb73b5d48dcf93d758f9c8847a11
divinity76 <hans@loltek.net>
2024-03-17 23:06:20 +0100

    PHP8.3 compatibility
    Turns out PHP<8.3's proc_open does not really care if the requested working_dir does not exist, while PHP>=8.3's proc_open bails if it does not exist.
    
    And due to a typo in the code, the dir never existed.
    
    And due to insufficient error checking, the error was not really detected.
    
    Took fking 4 hours to track down, and the fix is 1 byte. https://github.com/divinity76/wrench/pull/1
    
    Should probably complain to the php-src bugtracker, i am guessing the proc_open behavior change was unintentional

--- tests/Test/ServerTestHelper.php
+++ tests/Test/ServerTestHelper.php
@@ -111,9 +111,11 @@ final class ServerTestHelper implements LoggerAwareInterface
                 2 => ['file', $directory.'/server.err.log', 'a+'],
             ],
             $this->pipes,
-            __DIR__.'../'
+            __DIR__.'/../'
         );
-
+        if($this->process === false){
+            throw new \RuntimeException("proc_open failed: " . var_export(error_get_last(), true));
+        }
         \sleep(3);
     }
 
